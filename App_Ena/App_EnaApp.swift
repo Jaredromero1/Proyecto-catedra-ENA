cdda8ec13b2829078805cfe4448b1bf3ce08f3dd
Jared Romero <jaredromero08@gmail.com>
2024-10-07 21:58:51 -0600

    basic structure of the project

---
+++ App_Ena/App_EnaApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_EnaApp.swift
+//  App_Ena
+//
+//  Created by Jared Romero on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct App_EnaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
